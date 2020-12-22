06ac3a9cfcbc586cfbf26c5bf99491c4ac6168d4
WarrenGDM <WarrenGDM@gmail.com>
2020-12-22 15:52:40 +0900

    up

--- spring_6_1_ex1_1/src/main/java/com/javalec/ex/MainClass.java
+++ spring_6_1_ex1_1/src/main/java/com/javalec/ex/MainClass.java
@@ -4,10 +4,42 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class MainClass {
-	
 	public static void main(String[] args) {
-		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationCTX.xml");
-
+		/*	6강. DI설정 방법
+		 - XML파일을 이용한 DI설정 방법 (V)
+		 - JAVA를 이용한 DI설정 방법
+		 - XML과 JAVA를 같이 사용
+		 */
+		String configLocation1 = "classpath:applicationCTX.xml";
+		String configLocation2 = "classpath:applicationCTX2.xml";
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext(configLocation1, configLocation2);
+		Student student1 = ctx.getBean("student1", Student.class);
+		System.out.println(student1.getName());
+		System.out.println(student1.getHobbys());
+		
+		StudentInfo studentInfo = ctx.getBean("studentInfo1", StudentInfo.class);
+		Student student2 = studentInfo.getStudent();
+		System.out.println(student2.getName());
+		System.out.println(student2.getHobbys());
+		
+		if(student1.equals(student2)) {
+			System.out.println("student1 == student2");
+		}
+		
+		Student student3 = ctx.getBean("student3", Student.class);
+		System.out.println(student3.getName());
+		
+		if(student1.equals(student3)) {
+			System.out.println("student1 == student3");
+		} else {
+			System.out.println("student1 != student3");
+		}
+		
+		Family family = ctx.getBean("family", Family.class);
+		System.out.println(family.getPapaName());
+		System.out.println(family.getMamiName());
+		System.out.println(family.getSisterName());
+		System.out.println(family.getBrotherName());
 		
 		ctx.close();
 	}
